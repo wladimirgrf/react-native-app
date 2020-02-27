2812ac3f024476200b7ead97805828dfedf9c802
Wladimir <wladimir.code@gmail.com>
2020-02-26 23:05:05 -0300

    reactNativeApp, new project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+    </View>
+  );
+}
+
+export default App;
