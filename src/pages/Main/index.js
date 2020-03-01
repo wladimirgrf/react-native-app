d6f7d0e540be2c3f1f93d35ea3cfb74e49952da4
Wladimir <wladimir.code@gmail.com>
2020-02-29 23:23:22 -0300

    Navigation, react-navigation config, routes

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function Main() {
+  return <View />;
+}
+
+export default Main;
+
+Main.navigationOptions = {
+  title: 'Users',
+};
---
+++ src/pages/User/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function User() {
+  return <View />;
+}
+
+export default User;
