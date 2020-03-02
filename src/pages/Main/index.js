e281034bcacc0a86c243a6a600af89a437fd5604
Wladimir <wladimir.code@gmail.com>
2020-03-01 21:10:43 -0300

    Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { Container } from './styles';
+
 function Main() {
-  return <View />;
+  return <Container />;
 }
 
 export default Main;
