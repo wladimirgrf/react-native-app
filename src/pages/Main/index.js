8c534a5ddcef3835c14c06d9c3c28c6fec3f8db6
Wladimir <wladimir.code@gmail.com>
2020-03-01 21:41:24 -0300

    React Native Vector Icons, Form components and style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="New user"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
